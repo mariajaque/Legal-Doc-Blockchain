a82489c5d3bca5178a021a5ea5015be880dd20c3
Maria <202110874@alu.comillas.edu>
2025-04-30 14:53:55 -0400

    bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import artifact from "../../artifacts/contracts/LegalDocumentManager.sol/LegalDocumentManager.json";
+import artifact from "./abis/contracts/LegalDocumentManager.sol/LegalDocumentManager.json";
 import "./App.css";
 
 const CONTRACT_ADDRESS = import.meta.env.VITE_DOC_MANAGER;
